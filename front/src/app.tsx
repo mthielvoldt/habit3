46c3f7b8c0689dce7d657d7202eaad712ef7ae4b
mthielvoldt <mthielvoldt@gmail.com>
2024-05-30 13:38:53 -0400

    Install vite and react

---
+++ front/src/app.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function App() {
+  const [state, setState] = useState(0);
+
+  return (
+    <>
+      <h1>Hi!</h1>
+    </>
+  );
+}
+
+export default App;
