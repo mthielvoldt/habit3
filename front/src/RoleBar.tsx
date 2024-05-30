765798ac83b93c7f678bef576c3b96791bc26d39
mthielvoldt <mthielvoldt@gmail.com>
2024-05-30 15:06:10 -0400

    format whitespace

--- front/src/RoleBar.tsx
+++ front/src/RoleBar.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Role from './Role';
 
 function RoleBar({ roles, addRole }) {
-    return (
-        <aside id="sidebar" className="bg-light border-right">
-            <div className="sidebar-header p-3 border-bottom d-flex justify-content-between align-items-center">
-                <span>Roles</span>
-                <button className="btn btn-primary btn-sm" onClick={addRole}>Add Role</button>
-            </div>
-            <div className="p-3">
-                {roles.map((role, index) => (
-                    <Role key={index} roleTitle={role.roleTitle} rocks={role.rocks} />
-                ))}
-            </div>
-        </aside>
-    );
+  return (
+    <aside id="sidebar" className="bg-light border-right">
+      <div className="sidebar-header p-3 border-bottom d-flex justify-content-between align-items-center">
+        <span>Roles</span>
+        <button className="btn btn-primary btn-sm" onClick={addRole}>Add Role</button>
+      </div>
+      <div className="p-3">
+        {roles.map((role, index) => (
+          <Role key={index} roleTitle={role.roleTitle} rocks={role.rocks} />
+        ))}
+      </div>
+    </aside>
+  );
 };
 
 export default RoleBar;
