26b1b3dbc0c07d8e810a14039154f47d54c04403
mthielvoldt <mthielvoldt@gmail.com>
2024-05-30 15:47:50 -0400

    Move the "Add Rock" button to bottom of the list

--- front/src/RoleBar.tsx
+++ front/src/RoleBar.tsx
@@ -12,19 +12,19 @@ const RoleBar = ({ roles, addRole }) => {
     <aside id="sidebar" className="bg-light border-right">
       <div className="sidebar-header p-3 border-bottom d-flex justify-content-between align-items-center">
         <span>Roles</span>
-        <div>
-          <button className="btn btn-secondary btn-sm mr-2" onClick={toggleEditing}>
-            {isEditing ? 'Stop Editing' : 'Edit All'}
-          </button>
-          {isEditing && (
-            <button className="btn btn-primary btn-sm" onClick={addRole}>Add Role</button>
-          )}
-        </div>
+        <button className="btn btn-secondary btn-sm" onClick={toggleEditing}>
+          {isEditing ? 'Stop Editing' : 'Edit All'}
+        </button>
       </div>
       <div className="p-3">
         {roles.map((role, index) => (
           <Role key={index} roleTitle={role.roleTitle} rocks={role.rocks} isEditing={isEditing} />
         ))}
+        {isEditing && (
+          <div className="mt-3">
+            <button className="btn btn-primary btn-sm w-100" onClick={addRole}>Add Role</button>
+          </div>
+        )}
       </div>
     </aside>
   );
