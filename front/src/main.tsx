674729ddc06360be80e88205861bc25303562ce9
mthielvoldt <mthielvoldt@gmail.com>
2024-06-03 11:57:59 -0400

    fix type error in main.tsx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client'
 import App from './app.tsx'
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
