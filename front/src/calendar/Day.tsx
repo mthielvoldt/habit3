f810a574aef1ab6e04b13cd7febf58c1a82c3c8b
mthielvoldt <mthielvoldt@gmail.com>
2024-06-06 03:05:19 -0400

    Orange boxes moving across days with drag

--- front/src/calendar/Day.tsx
+++ front/src/calendar/Day.tsx
@@ -2,19 +2,40 @@ import React, {useState} from "react";
 import { useDrop } from "react-dnd";
 
 export default function Day({day}) {
-  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+  const [yValue, setYValue] = useState(12);
+  const [{ isOver }, drop] = useDrop(() => ({
     // The type (or types) to accept - strings or symbols
     accept: 'rock',
     // Props to collect
     collect: (monitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop()
-    })
+    }),
+    drop: handleDrop
   }))
 
+  function handleDrop(item, monitor) {
+    console.log(yValue);
+    console.log(day.dayOfWeek)
+    console.log(item);
+    console.log(monitor);
+  }
+
+  function handleMouseOver(event) {
+    if (event.clientY !== 0) {
+      setYValue(event.clientY);
+    }
+  }
+
   return (
-    <div className="day" ref={drop} style={{ backgroundColor: isOver ? 'red' : 'white'}}>
-      {canDrop ? 'Release to drop' : 'drag a rock here'}
+    <div
+      className="day"
+      ref={drop}
+      onDragOverCapture={handleMouseOver}
+    >
+      <div className="shadow-event"
+        style={{top: yValue.toString()+'px', visibility: isOver ? 'visible' : 'hidden'}}>
+      </div>
     </div>
   );
 }
