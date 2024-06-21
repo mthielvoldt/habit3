36a3d10d796bcbd2d232b4d7c5d6b69b7bed3766
mthielvoldt <mthielvoldt@gmail.com>
2024-06-21 16:13:30 -0400

    Make delete buttons smaller in rocks and roles

--- front/src/Role.tsx
+++ front/src/Role.tsx
@@ -16,21 +16,24 @@ const Role = ({ role, isEditing, dispatch }) => {
   return (
     <div className="role mb-3">
       <div className="role-header d-flex justify-content-between align-items-center mb-2">
-        {isEditing ? (
-          <input
-            type="text"
-            value={role.roleTitle}
-            onChange={handleTitleChange}
-            className="form-control"
-          />
-        ) : (
+        {isEditing ?
+          <div className='input-group'>
+            <input
+              type="text"
+              value={role.roleTitle}
+              onChange={handleTitleChange}
+              className="form-control"
+            />
+            <button className="btn btn-danger btn-sm ml-2" onClick={() => dispatch(deleteRoleAction(role.id))}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+              </svg>
+            </button>
+          </div>
+          :
           <span className="font-weight-bold">{role.roleTitle}</span>
-        )}
-        {isEditing && (
-          <button className="btn btn-danger btn-sm ml-2" onClick={() => dispatch(deleteRoleAction(role.id))}>
-            Delete Role
-          </button>
-        )}
+        }
       </div>
       <div className="role-rocks">
         {role.rocks.map(rock => (
